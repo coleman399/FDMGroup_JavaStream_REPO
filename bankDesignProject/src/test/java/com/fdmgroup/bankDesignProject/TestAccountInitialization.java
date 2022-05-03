2c7f11e0b54c4e352dd02d0d4d0ecb4578324dcc
dillon.coleman <coleman399@gmail.com>
2022-05-02 19:04:56 -0700

    cleaned house

--- bankDesignProject/src/test/java/com/fdmgroup/bankDesignProject/TestAccountInitialization.java
+++ bankDesignProject/src/test/java/com/fdmgroup/bankDesignProject/TestAccountInitialization.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestAccountInitialisation {
+public class TestAccountInitialization {
 
 	Account savingsAccount1;
 	Account savingsAccount2;
--- bankDesignProject/src/test/java/com/fdmgroup/bankDesignProject/TestCompany.java
+++ bankDesignProject/src/test/java/com/fdmgroup/bankDesignProject/TestCompany.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestCompany {
-	
+
 	Company company;
 	CheckingAccount checkingAccount1;
 	CheckingAccount checkingAccount2;
@@ -15,7 +15,7 @@ public class TestCompany {
 
 	@Before
 	public void setUp() throws Exception {
-		company = new Company("company1","address1");
+		company = new Company("company1", "address1");
 		checkingAccount1 = new CheckingAccount();
 		checkingAccount2 = new CheckingAccount();
 		savingsAccount1 = new SavingsAccount();
@@ -25,20 +25,20 @@ public class TestCompany {
 		company.addAccount(savingsAccount1);
 		company.addAccount(savingsAccount2);
 	}
- 
+
 	@Test
 	public void test_chargeAllAccountsWorks() {
 		checkingAccount1.correctBalance(10000);
 		checkingAccount2.correctBalance(7500);
 		savingsAccount1.correctBalance(150000);
 		savingsAccount2.correctBalance(250000);
-		
+
 		company.chargeAllAccounts(10);
-		
-		assertEquals(9990,checkingAccount1.getBalance(),0.00001);
-		assertEquals(7490,checkingAccount2.getBalance(),0.00001);
-		assertEquals(149980,savingsAccount1.getBalance(),0.00001);
-		assertEquals(249980,savingsAccount2.getBalance(),0.00001);
+
+		assertEquals(9990, checkingAccount1.getBalance(), 0.00001);
+		assertEquals(7490, checkingAccount2.getBalance(), 0.00001);
+		assertEquals(149980, savingsAccount1.getBalance(), 0.00001);
+		assertEquals(249980, savingsAccount2.getBalance(), 0.00001);
 	}
 
 }
